27d70d95477ba6374515cbf2cb2f2caaa156f474
Hector Leiva <hector@hectorleiva.com>
2020-09-01 21:29:07 -0400

    including download_dir env var

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ if not PLAYLIST_URL:
     print('Unable to continue, no YOUTUBE_PLAYLIST_URL environment variable was set')
     sys.exit(1)
 
+DOWNLOAD_DIR = os.getenv('DOWNLOAD_DIR')
+if not DOWNLOAD_DIR:
+    print('Unable to continue, no DOWNLOAD_DIR environment variable was set')
+    sys.exit(1)
+
 # Currently unable to download more than one video at a time
 youtube_downloader(
     [PLAYLIST_URL
      , '--ignore-errors'
      , '--download-archive'
-     , '{}/archive.txt'.format(HERE + '/downloaded-videos')
+     , '{}/archive.txt'.format(DOWNLOAD_DIR)
      , '-r'
      , '2M'
-     , '-o {}/%(title)s-%(id)s.%(ext)s'.format(HERE + '/downloaded-videos')]
+     , '-o {}/%(title)s-%(id)s.%(ext)s'.format(DOWNLOAD_DIR)]
 )
