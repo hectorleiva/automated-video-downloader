6c5dad99e97c1e7c8a60ce1b5132d8b70074401f
Hector Leiva <hector@hectorleiva.com>
2020-09-05 21:09:34 -0400

    adding more documentation and stream lining the main file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # automated-video-downloader
 A simple program that leverages the youtube-dl program to automate downloading of specific video content to another location
+
+Despite the name of the library used, [youtube-dl](https://youtube-dl.org/) is capable of downloading from [many different video streams](https://ytdl-org.github.io/youtube-dl/supportedsites.html).
+
+## .env
+For example
+```bash
+PLAYLIST_URL=https://www.youtube.com/playlist?list=xxxxxxxxxxxx
+```
+
+The `PLAYLIST_URL` must be accessible without authorization for this to work.
--- main.py
+++ main.py
@@ -6,17 +6,13 @@ sys.path.append(os.path.join(HERE, "./vendored"))
 
 from youtube_dl import main as youtube_downloader
 
-PLAYLIST_URL = os.getenv('YOUTUBE_PLAYLIST_URL')
+PLAYLIST_URL = os.getenv('PLAYLIST_URL')
 if not PLAYLIST_URL:
-    print('Unable to continue, no YOUTUBE_PLAYLIST_URL environment variable was set')
+    print('Unable to continue, no PLAYLIST_URL environment variable was set')
     sys.exit(1)
 
-DOWNLOAD_DIR = os.getenv('DOWNLOAD_DIR')
-if not DOWNLOAD_DIR:
-    print('Unable to continue, no DOWNLOAD_DIR environment variable was set')
-    sys.exit(1)
+DOWNLOAD_DIR = 'download-videos'
 
-# Currently unable to download more than one video at a time
 youtube_downloader(
     [PLAYLIST_URL
      , '--ignore-errors'
@@ -24,5 +20,6 @@ youtube_downloader(
      , '{}/archive.txt'.format(DOWNLOAD_DIR)
      , '-r'
      , '2M'
-     , '-o {}/%(title)s-%(id)s.%(ext)s'.format(DOWNLOAD_DIR)]
+     , '-o'
+     , '{}/%(title)s-%(id)s.%(ext)s'.format(DOWNLOAD_DIR)]
 )
